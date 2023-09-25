a3c53f45a4648bba594957d93a94ddc61f6d08d0
NiceLeader <maciek2002.10@wp.pl>
2023-09-25 13:47:22 +0200

    ini

---
+++ src/main/java/dev/niceleader/gym_buddy_connect/Application.java
@@ -0,0 +1,13 @@
+package dev.niceleader.gym_buddy_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/niceleader/gym_buddy_connect/GymBuddyConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.niceleader.gym_buddy_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBuddyConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
