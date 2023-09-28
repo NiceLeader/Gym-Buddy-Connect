0031c76d8bd6738fca620edbc0d160bbe4f03319
NiceLeader <maciek2002.10@wp.pl>
2023-09-28 14:51:11 +0200

    .

--- src/main/java/dev/niceleader/gym_buddy_connect/controller/ClubController.java
+++ src/main/java/dev/niceleader/gym_buddy_connect/controller/ClubController.java
@@ -1,7 +1,12 @@
 package dev.niceleader.gym_buddy_connect.controller;
 
+import dev.niceleader.gym_buddy_connect.dto.ClubDto;
 import dev.niceleader.gym_buddy_connect.logic.service.ClubService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ClubController {
@@ -9,6 +14,13 @@ public class ClubController {
 
     public ClubController(ClubService clubService) {
         this.clubService = clubService;
+    }
+
+    @GetMapping("/clubs")
+    String listClubs(Model model) {
+        List<ClubDto> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs", clubs);
+        return "clubs-list";
     }
 
 }
